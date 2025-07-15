bed8edae7f875474448b9cc60a8c88650b71f618
Thangbodoiqua <dluy2406@gmail.com>
2025-07-15 21:32:29 +0700

    backend - initialization

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
-
+import Navigation from './customer/components/Navigation'
 function App() {
 
   return (
     <>
       <h1 className='text-3xl font-bold'>E-commerce App</h1>
+      <Navigation />
     </>
   )
 }
